7c69f4a87a4e354667784ed9d5521628457efea1
sanjeev <sanjeevsingh.77d@gmail.com>
2022-12-18 18:30:12 +0530

    Check file exists or Not

---
+++ File_IO_Opeerations/File_IO_Opeerations/Operations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_IO_Opeerations
+{
+    public class Operations
+    {
+        public void FileExists()
+        {
+            string filePath = @"F:\Bridgelabz Codin\FileIOOperations\FileIO_Operations\FileIO_Operations\WordFile1.txt";
+            if (File.Exists(filePath))
+            {
+                Console.WriteLine("\nFile Exists");
+            }
+            else
+            {
+                Console.WriteLine("\nFile Not Found");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ File_IO_Opeerations/File_IO_Opeerations/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace File_IO_Opeerations
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("================== Welcome to File Input-Output Operation ================");
+
+            Operations obj = new Operations();
+            obj.FileExists();
+        }
+    }
+}
