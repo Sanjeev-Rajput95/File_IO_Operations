eb6252b23fbda184a523185d0af3ba2033220751
sanjeev <sanjeevsingh.77d@gmail.com>
2022-12-18 18:39:26 +0530

    Read All Lines from a text file

--- File_IO_Opeerations/File_IO_Opeerations/Operations.cs
+++ File_IO_Opeerations/File_IO_Opeerations/Operations.cs
@@ -10,7 +10,8 @@ namespace File_IO_Opeerations
     {
         public void FileExists()
         {
-            string filePath = @"F:\Bridgelabz Codin\FileIOOperations\FileIO_Operations\FileIO_Operations\WordFile1.txt";
+            Console.WriteLine("\n============ File Exists Operations ============ \n");
+            string filePath = @"C:\Bridgelabz\File_IO_Operations\File_IO_Operations\File_IO_Opeerations\File_IO_Opeerations\WorldFile1.txt";
             if (File.Exists(filePath))
             {
                 Console.WriteLine("\nFile Exists");
@@ -19,6 +20,16 @@ namespace File_IO_Opeerations
             {
                 Console.WriteLine("\nFile Not Found");
             }
+        }
+
+        public void ReadAllLines()
+        {
+            Console.WriteLine("\n**********Read All Lines**********\n");
+            string filePath = @"C:\Bridgelabz\File_IO_Operations\File_IO_Operations\File_IO_Opeerations\File_IO_Opeerations\WorldFile1.txt";
+            string[] lines;
+            lines = File.ReadAllLines(filePath);
+            Console.WriteLine(lines[0]);
+            Console.WriteLine(lines[1]);
             Console.ReadLine();
         }
     }
